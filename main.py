1158d3324c4b08381b8be2948bdacce329f29db6
Akimosabe <he7seven@gmail.com>
2024-06-22 23:46:35 +0500

    added author's note and text, added logging

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import os
 import sys
 import threading
 import time
+import logging
 import telebot
 import vk_api
 from telebot import types
+
+# Настройка логгирования
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%d.%m.%Y %H:%M",
+)
 
 # Чтение конфигурационного файла JSON
 with open("config.json", "r") as f:
@@ -26,29 +34,27 @@ module = sys.modules[__name__]
 # Функция для инициализации Telegram бота
 def start_tg():
     module.bot = telebot.TeleBot(telegram_token)
-    print("Телеграм инициализирован.")
+    logging.info("Телеграм инициализирован.")
 
 
 # Функция для инициализации VK API
 def start_vk():
     vk_session = vk_api.VkApi(token=vk_token)
     module.vk = vk_session.get_api()
-    print("ВК инициализирован.")
+    logging.info("ВК инициализирован.")
 
     # Запуск функции проверки новостной ленты
     check_news(int(time.time()))
 
 
 # Функция для проверки новостной ленты VK
-def check_news(start_time):
+def check_news(start_time: int):
     retries = 0
     while True:
         time.sleep(time_check)  # Задержка между проверками
         try:
-            newsfeed = module.vk.newsfeed.get(
-                count=100, start_time=start_time, max_photos=10
-            )
-            posts = (json.loads(json.dumps(newsfeed))).get("items")
+            newsfeed = module.vk.newsfeed.get(count=100, start_time=start_time, max_photos=10)
+            posts = newsfeed.get("items")
             if posts:
                 start_time = posts[0]["date"] + 1
                 for post in posts[::-1]:
@@ -56,43 +62,32 @@ def check_news(start_time):
             retries = 0  # Сбросить счетчик повторных попыток при успешном запросе
         except Exception as e:
             retries += 1
-            print(f"Ошибка при проверке новостной ленты: {e}")
+            logging.error(f"Ошибка при проверке новостной ленты: {e}")
             if retries >= retries_max:
-                print("Превышено максимальное количество повторных попыток. Завершение программы.")
+                logging.error("Превышено максимальное количество повторных попыток. Завершение программы.")
                 break
-            print(f"Повторная попытка через {retries_time} секунд (попытка {retries}/{retries_max})")
+            logging.info(f"Повторная попытка через {retries_time} секунд (попытка {retries}/{retries_max})")
             time.sleep(retries_time)
 
 
 # Функция для проверки прикреплений в посте VK
-def check_content(post):
+def check_content(post: dict):
     if post.get("photos"):
         return
 
     if post.get("copy_history"):
         post = post["copy_history"][0]
 
-    if not (post.get("attachments")):
-        pass
-    else:
+    if post.get("attachments"):
         send_content(get_content(post))
 
 
 # Функция для получения URL изображения определенного размера
-def scale(size_path):
-    photo_size = None
-
-    for photoType in size_path[0:]:
-        if photoType.get("type") == "x":
-            photo_size = photoType.get("url")
-        if photoType.get("type") == "y":
-            photo_size = photoType.get("url")
-        if photoType.get("type") == "z":
-            photo_size = photoType.get("url")
-        if photoType.get("type") == "w":
-            photo_size = photoType.get("url")
-
-    return photo_size
+def scale(size_path: list) -> str:
+    for photoType in size_path:
+        if photoType.get("type") in {"x", "y", "z", "w"}:
+            return photoType.get("url")
+    return None
 
 
 # Функция для получения материалов из поста VK
@@ -100,7 +95,23 @@ def get_content(post):
     attach_list = []
     photo_group = []
 
-    for att in post["attachments"][0:]:
+    # Определяем автора поста
+    if post.get("source_id") > 0:
+        # Пост от пользователя
+        author_info = module.vk.users.get(user_ids=post.get("source_id"))[0]
+        author = f"{author_info.get('first_name')} {author_info.get('last_name')}"
+    else:
+        # Пост от сообщества или паблика
+        group_info = module.vk.groups.getById(group_ids=abs(post.get("source_id")))[0]
+        author = group_info.get("name")
+
+    # Получаем текст поста, если он есть
+    text = post.get("text", "")
+
+    # Добавляем автора и текст в начало сообщения
+    comment = f"Источник: **{author}**\n\n{text}\n"
+
+    for att in post["attachments"]:
         att_type = att.get("type")
         attachment = att[att_type]
 
@@ -132,86 +143,73 @@ def get_content(post):
 
         elif att_type == "doc":
             title = attachment.get("title")
-            doc_type = attachment.get("type")
-            if doc_type != 3 and doc_type != 4 and doc_type != 5:
+            if attachment.get("type") in {3, 4, 5}:
+                attachments = attachment.get("url")
+            else:
                 att_type = "other"
-            attachments = attachment.get("url")
+                attachments = attachment.get("url")
 
         elif att_type == "album":
             preview = scale(attachment["thumb"].get("sizes"))
             title = attachment.get("title")
-            owner_id = str(attachment.get("owner_id"))
-            album_id = str(attachment.get("id"))
-            attachments = str(f"https://vk.com/album{owner_id}_{album_id}")
+            attachments = f"https://vk.com/album{attachment.get('owner_id')}_{attachment.get('id')}"
 
         elif att_type == "link" and attachment.get("description") == "Статья":
             preview = scale(attachment["photo"].get("sizes"))
             title = attachment.get("title")
-            attachments = str(attachment.get("url"))
+            attachments = attachment.get("url")
 
-        if attachments is not None:
-            attach_list.append(
-                {
-                    "type": att_type,
-                    "link": attachments,
-                    "title": title,
-                    "preview": preview,
-                }
-            )
+        if attachments:
+            attach_list.append({
+                "type": att_type,
+                "link": attachments,
+                "title": title,
+                "preview": preview,
+                "comment": comment  
+            })
 
     if photo_group:
-        attach_list.append({"type": "photo", "link": photo_group})
+        attach_list.append({"type": "photo", "link": photo_group, "comment": comment})
 
     return attach_list
 
-
 # Функция для отправки в Telegram
 def send_content(attachments):
-    for attach_element in attachments[0:]:
+    for attach_element in attachments:
         att_type = attach_element.get("type")
         link = attach_element.get("link")
         title = attach_element.get("title")
         preview = attach_element.get("preview")
+        comment = attach_element.get("comment")  # Получаем комментарий
 
         try:
             if att_type == "photo":
-                media_photo = []
-                for photo_url in link[0:]:
-                    media_photo.append(types.InputMediaPhoto(photo_url))
+                media_photo = [types.InputMediaPhoto(photo_url, caption=comment) for photo_url in link]
                 module.bot.send_media_group(telegram_chat, media_photo)
-                print("Изображение отправлено.")
+                logging.info("Изображение отправлено.")
 
             elif att_type == "video":
-                module.bot.send_media_group(
-                    telegram_chat,
-                    [types.InputMediaPhoto(preview, caption=f"{title}\n{link}")],
-                )
-                print("Видео отправлено.")
+                module.bot.send_media_group(telegram_chat,[types.InputMediaPhoto(preview, caption=f"{comment}\n{link}")])
+                logging.info("Видео отправлено.")
 
             elif att_type == "album":
-                module.bot.send_media_group(
-                    telegram_chat,
-                    [types.InputMediaPhoto(preview, caption=f"{title}\n{link}")],
-                )
-                print("Альбом отправлен.")
+                module.bot.send_media_group(telegram_chat, [types.InputMediaPhoto(preview, caption=f"{comment}\n{title}\n{link}")])
+                logging.info("Альбом отправлен.")
 
             elif att_type == "link":
-                module.bot.send_media_group(
-                    telegram_chat,
-                    [types.InputMediaPhoto(preview, caption=f"{title}\n{link}")],
-                )
-                print("Ссылка отправлена.")
+                module.bot.send_message(telegram_chat, f"{comment}\n{title}\n{link}")
+                logging.info("Ссылка отправлена.")
 
-            elif att_type == "doc" or att_type == "gif":
-                module.bot.send_document(telegram_chat, link)
-                print("Документ отправлен.")
+            elif att_type in {"doc", "gif"}:
+                module.bot.send_document(telegram_chat, link, caption=comment)
+                logging.info("Документ отправлен.")
 
             elif att_type == "other":
-                module.bot.send_message(telegram_chat, f"{title}\n{link}")
-                print("Отправлено.")
+                module.bot.send_message(telegram_chat, f"{comment}\n{title}\n{link}")
+                logging.info("Отправлено.")
 
         except Exception as e:
-            pass
+            logging.error(f"Ошибка при отправке сообщения в Telegram: {e}")
 
 
 # Запуск инициализации VK и Telegram в отдельных потоках
